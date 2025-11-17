20d0e20fed23bd112984ac2cd02a9060457e00d3
lucianoleroi <tomas-luciano-antonio.irigoyen-barberis@epitech.digital>
2025-11-17 20:34:15 +0100

    msg

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/lucianoleroi/Desktop/carthia/archivos md copy/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vSutxm+88yhcLYwR+HTcrpihqQIT0VJp0ogMUJJyNpw=\"\n}"
